51df1e73da9c3e60aa9a20e1d6cf2e9013c9a911
houjingl <jerryhou2005@outlook.com>
2025-02-06 20:50:24 -0500

    FL2 update

---
+++ FL2/FL2_PLL.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=FL2_PLL.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/FL2_PLL.X.production.hex
